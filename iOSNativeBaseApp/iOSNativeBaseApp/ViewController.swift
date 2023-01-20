445b2defbd16a86584380be7cc0a656bc2716a8f
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-20 12:32:21 +0530

    setup app

---
+++ iOSNativeBaseApp/iOSNativeBaseApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  iOSNativeBaseApp
+//
+//  Created by Jeevan Chandra Joshi on 20/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
