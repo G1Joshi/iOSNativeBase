6a9737ff443fb9bc350451f57967f827ef976db2
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-20 12:42:05 +0530

    setup framework

---
+++ iOSNativeBaseFramework/iOSNativeBaseFramework/iOSNativeBaseFramework.h
@@ -0,0 +1,16 @@
+//
+//  iOSNativeBaseFramework.h
+//  iOSNativeBaseFramework
+//
+//  Created by Jeevan Chandra Joshi on 20/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSNativeBaseFramework.
+FOUNDATION_EXPORT double iOSNativeBaseFrameworkVersionNumber;
+
+//! Project version string for iOSNativeBaseFramework.
+FOUNDATION_EXPORT const unsigned char iOSNativeBaseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSNativeBaseFramework/PublicHeader.h>
